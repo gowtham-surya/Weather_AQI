35214ef2cd68e257806c2329a08e2ce90c22117b
gowtham-surya <gowthamsurya95855@gmail.com>
2022-08-28 04:17:40 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Entry from './components/Entry';
 import Port_A from './components/Port_A';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Entry} />
-        <Route path="/port_A" component={Port_A} />
-        <Route path="/port_B" component={Port_A} />
-        <Route path="/port_C" component={Port_A} />
-      </Switch>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" exact component={Entry} />
+      <Route path="/port_A" component={Port_A} />
+      <Route path="/port_B" component={Port_A} />
+      <Route path="/port_C" component={Port_A} />
+    </Routes>
   );
 };
 
